e413823a1cc509e978ee1a1a794ebd389e61f2bb
John Christian Idul <idulchristian@gmail.com>
2023-12-04 08:18:32 +0800

    deleted card copy folder added the scroll view margin to minimize space between header and card

--- components/Forecast/cards/ForecastSaleCard.js
+++ components/Forecast/cards/ForecastSaleCard.js
@@ -1,22 +1,25 @@
-import { View, StyleSheet, Dimensions, Image } from "react-native";
+import { View, StyleSheet, StatusBar, Image, TouchableOpacity } from "react-native";
 import React from "react";
 import { Avatar, Button, Card, Text } from "react-native-paper";
-import { COLORS, SIZES, FONT } from "../../../constants";
-import styles from "./styles/Forecast.style";
-
+import { COLORS, FONT, SIZES } from "../../../constants";
+import styles from "./styles/ForecastSale.style";
 const ForecastSaleCard = () => {
+
   return (
+    <TouchableOpacity>
     <Card style={styles.container}>
-      <Card.Content contentStyle={{ }}>
-      <Text style={[styles.textStyle, {fontFamily: FONT.bold, fontSize: SIZES.large}]}>Quarterly Sales</Text>
-      <Text style={[styles.textStyle, {marginTop: 5, marginBottom: 10, fontFamily: FONT.medium, fontSize: SIZES.xxLarge}]}>$XX,XXX.XX</Text>
+      <Card.Content contentStyle={styles.container}>
+        <Text style={[styles.textStyle, {fontFamily: FONT.bold, fontSize: SIZES.medium}]}>Quarterly sales</Text>
+        <Text style={[styles.textStyle, {marginTop: 5, marginBottom: 10, fontFamily: FONT.medium, fontSize: SIZES.xLarge}]}>$27,XXX.XX</Text>
       </Card.Content>
       <Image
         source={require("../../../assets/images/forecast/forecast-card-image.png")}
         style={styles.image}
       />
     </Card>
+    </TouchableOpacity>
   );
 };
+
 
 export default ForecastSaleCard;
