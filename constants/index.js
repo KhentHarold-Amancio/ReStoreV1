327a93a94aff8a63ea1206d2ebabb2b7b3f4507e
Khent Harold Amancio <76564829+KhentHarold-Amancio@users.noreply.github.com>
2023-12-07 05:15:25 +0800

    Added import button func

--- constants/index.js
+++ constants/index.js
@@ -1,6 +1,7 @@
 import images from "./image";
 import { COLORS, FONT, SIZES, SHADOWS } from "./theme";
 
-const ENDPOINT = 'http://192.168.254.116:5000/';
+const ENDPOINT = "http://192.168.254.116:5000/";
+const ENDPNTUPLOAD = "http://192.168.254.116:5000/upload";
 
-export { images, COLORS, FONT, SIZES, SHADOWS, ENDPOINT };
+export { images, COLORS, FONT, SIZES, SHADOWS, ENDPOINT, ENDPNTUPLOAD };
